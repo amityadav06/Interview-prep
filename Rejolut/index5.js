6abadf91bda9937460b500318a3b83b2f5dca230
amityadav06 <amityadav06071998@gmail.com>
2024-11-24 11:30:50 +0530

    new ques added

--- Rejolut/index5.js
+++ Rejolut/index5.js
@@ -5,4 +5,37 @@
 // ds questions
 // html css
 
+// const arr = [
+//     {
+//         name: 'amit'
+//     },
+//     {
+//         name: 'aman'
+//     },
+//     {
+//         name: 'sumit'
+//     },
+//     {
+//         name: 'shyam'
+//     },
+//     {
+//         name: 'amit'
+//     },
+// ]
 
+// function returnUnique(arr){
+    
+//     const seen = {};
+//     const uniqueArr = [];
+    
+//     arr.forEach(item => {
+//         if (!seen[item.name]) {
+//             seen[item.name] = true;
+//             uniqueArr.push(item);
+//         }
+//     });
+        
+//     return uniqueArr;
+// }
+
+// console.log(returnUnique(arr));
